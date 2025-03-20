e64600895293d6c416149d8a265efa6739229069
Tiago <tiagomendon45@gmail.com>
2025-03-20 13:34:04 -0300

    v3

--- ex115/desafio115.py
+++ ex115/desafio115.py
@@ -1,15 +1,25 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not ArquivoExiste(arq):
+    CriarArquivo(arq)
+
 print(cabecalho('Sistema de Arquivos'))
 while True:
     resposta = menu(['VER PESSOAS CADASTRADAS', 'CADASTRAR NOVA PESSOA', 'SAIR DO SISTEMA'])
     if resposta == 1:
-        cabecalho('VER PESSOAS CADASTRADAS')
+        LerArquivo(arq)
     elif resposta == 2:
         cabecalho('CADASTRAR NOVA PESSOA')
+        nome = str(input('Nome:'))
+        idade =leiaInt('Idade:')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
-        cabecalho('SAINDO DO SISTEMA .... ATÉ LOGO')
-        break
+            cabecalho('SAINDO DO SISTEMA .... ATÉ LOGO')
+            break
     else:
         print('\033[31mERRO:  digite uma opção valida!!!\033[m')
 
---
+++ ex115/lib/arquivo/__init__.py
@@ -0,0 +1,50 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+def ArquivoExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def CriarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')  # wt+ , CRIA UM ARQUIVO DE TEXTO
+        a.close()
+    except:
+        print('Houve um erro na criacao do arquivo!')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+def LerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um erro na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um erro na hora de escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+            a.close()
